f5cdb42a402f490b6d6bd6d8b9b03107718f20fe
henryruhs <info@henryruhs.com>
2023-01-02 00:36:19 +0100

    Extend the obsolete rules

--- src/rules/no-obsolete/option.ts
+++ src/rules/no-obsolete/option.ts
@@ -7,6 +7,12 @@ export const defaultOptions : Options =
 		{
 			name:
 			{
+				search: 'apply'
+			}
+		},
+		{
+			name:
+			{
 				search: 'document'
 			}
 		},
@@ -19,6 +25,56 @@ export const defaultOptions : Options =
 	],
 	properties:
 	[
+		{
+			name:
+			{
+				search: 'box-align'
+			}
+		},
+		{
+			name:
+			{
+				search: 'box-direction',
+				replace: 'flex-direction'
+			}
+		},
+		{
+			name:
+			{
+				search: 'box-flex'
+			}
+		},
+		{
+			name:
+			{
+				search: 'box-flex-group'
+			}
+		},
+		{
+			name:
+			{
+				search: 'box-lines'
+			}
+		},
+		{
+			name:
+			{
+				search: 'box-ordinal-group',
+				replace: 'order'
+			}
+		},
+		{
+			name:
+			{
+				search: 'box-orient'
+			}
+		},
+		{
+			name:
+			{
+				search: 'box-pack'
+			}
+		},
 		{
 			name:
 			{
@@ -60,7 +116,53 @@ export const defaultOptions : Options =
 		{
 			name:
 			{
+				search: 'image-orientation'
+			},
+			value:
+			{
+				search: 'flip'
+			}
+		},
+		{
+			name:
+			{
+				search: 'image-rendering'
+			},
+			value:
+			{
+				search: 'optimizeQuality',
+				replace: 'smooth'
+			}
+		},
+		{
+			name:
+			{
+				search: 'image-rendering'
+			},
+			value:
+			{
+				search: 'optimizeSpeed',
+				replace: 'pixelated'
+			}
+		},
+		{
+			name:
+			{
 				search: 'ime-mode'
+			}
+		},
+		{
+			name:
+			{
+				search: 'page-break-after',
+				replace: 'break-after'
+			}
+		},
+		{
+			name:
+			{
+				search: 'page-break-before',
+				replace: 'break-before'
 			}
 		},
 		{
@@ -78,18 +180,6 @@ export const defaultOptions : Options =
 		{
 			name:
 			{
-				search: 'scroll-snap-type-x'
-			}
-		},
-		{
-			name:
-			{
-				search: 'scroll-snap-type-y'
-			}
-		},
-		{
-			name:
-			{
 				search: 'scroll-snap-points-x'
 			}
 		},
@@ -102,7 +192,29 @@ export const defaultOptions : Options =
 		{
 			name:
 			{
+				search: 'scroll-snap-type-x'
+			}
+		},
+		{
+			name:
+			{
+				search: 'scroll-snap-type-y'
+			}
+		},
+		{
+			name:
+			{
 				search: 'text-decoration'
+			},
+			value:
+			{
+				search: 'blink'
+			}
+		},
+		{
+			name:
+			{
+				search: 'text-decoration-line'
 			},
 			value:
 			{
@@ -123,7 +235,7 @@ export const defaultOptions : Options =
 		{
 			name:
 			{
-				search: 'zoom'
+				search: 'user-modify'
 			}
 		}
 	]
--- tests/no-obsolete.spec.ts
+++ tests/no-obsolete.spec.ts
@@ -10,22 +10,38 @@ describe('no-obsolete', () =>
 	});
 
 	[
+		'Unexpected "apply" at-rule',
 		'Unexpected "document" at-rule',
 		'Unexpected "viewport" at-rule',
+		'Unexpected "box-align" property',
+		'Expected "box-direction" property to be "flex-direction"',
+		'Unexpected "box-flex" property',
+		'Unexpected "box-flex-group" property',
+		'Unexpected "box-lines" property',
+		'Expected "box-ordinal-group" property to be "order"',
+		'Unexpected "box-orient" property',
+		'Unexpected "box-pack" property',
 		'Unexpected "box-sizing" value "padding-box"',
 		'Expected "clip" property to be "clip-path"',
 		'Expected "grid-column-gap" property to be "column-gap"',
 		'Expected "grid-gap" property to be "gap"',
 		'Expected "grid-row-gap" property to be "row-gap"',
+		'Unexpected "image-orientation" value "flip"',
+		'Expected "image-rendering" value "optimizeQuality" to be "smooth"',
+		'Expected "image-rendering" value "optimizeSpeed" to be "pixelated"',
 		'Unexpected "ime-mode" property',
+		'Expected "page-break-after" property to be "break-after"',
+		'Expected "page-break-before" property to be "break-before"',
 		'Unexpected "scroll-snap-coordinate" property',
 		'Unexpected "scroll-snap-destination" property',
-		'Unexpected "scroll-snap-type-x" property',
-		'Unexpected "scroll-snap-type-y" property',
 		'Unexpected "scroll-snap-points-x" property',
 		'Unexpected "scroll-snap-points-y" property',
+		'Unexpected "scroll-snap-type-x" property',
+		'Unexpected "scroll-snap-type-y" property',
+		'Unexpected "text-decoration" value "blink"',
+		'Unexpected "text-decoration-line" value "blink"',
 		'Expected "word-break" value "break-word" to be "break-all"',
-		'Unexpected "zoom" property'
+		'Unexpected "user-modify" property'
 	]
 	.map((message, index) =>
 	{
