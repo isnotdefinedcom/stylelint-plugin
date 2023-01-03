1d30c1fdeaf0b926f1a1182a3f214d5fe174be9c
henryruhs <info@henryruhs.com>
2023-01-03 14:00:49 +0100

    Define commands without imports

--- src/rules/no-disable/option.ts
+++ src/rules/no-disable/option.ts
@@ -1,17 +1,11 @@
 import { Options } from './option.interface';
-import
-{
-	DISABLE_COMMAND,
-	DISABLE_LINE_COMMAND,
-	DISABLE_NEXT_LINE_COMMAND
-} from 'stylelint/lib/utils/stylelintCommand';
 
 export const defaultOptions : Options =
 {
 	commands:
 	[
-		DISABLE_COMMAND,
-		DISABLE_LINE_COMMAND,
-		DISABLE_NEXT_LINE_COMMAND
+		'stylelint-disable',
+		'stylelint-disable-line',
+		'stylelint-disable-next-line'
 	]
 };
